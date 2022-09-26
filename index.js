f1e5cbc467ca2228d28a80d9a691ccad3e271748
strangeal <bajerijustice@gmail.com>
2022-09-26 06:58:32 -0400

    Add files in modules

--- index.js
+++ index.js
@@ -1,61 +1,14 @@
-import { timeDate, date } from './modules/date.js';
-import * as nav from './modules/nav.js';
-// import * as main from './modules/class.js'
+import { timeDate, date } from './modules/date.js';  // eslint-disable-line
+import * as nav from './modules/nav.js';  // eslint-disable-line
 
 const bookListEl = document.querySelector('#book-list');
 const addBtn = document.querySelector('.btn');
 const titleEl = document.querySelector('#title');
 const authorEl = document.querySelector('#author');
 
-// display date & time dynamically
-// const timeDate = () => {
-//   const newDate = new Date();
-//   date.innerHTML = `${newDate.toDateString()}, ${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()}pm `;
-//   setTimeout(timeDate, 1000);
-// };
-// window.onload = timeDate();
-
-// const refresh = () => {
-//   contactMenu.style.display = 'none';
-//   formMenu.style.display = 'none';
-//   listMenu.style.display = 'block';
-//   heading.style.display = 'block';
-// };
-// window.onload = refresh();
-
-// contactEl.addEventListener('click', () => {
-//   contactMenu.style.display = 'block';
-//   formMenu.style.display = 'none';
-//   listMenu.style.display = 'none';
-//   heading.style.display = 'none';
-//   Link.style.color = '#000';
-//   contactEl.style.color = 'blue';
-//   formLink.style.color = '#000';
-// });
-
-// Link.addEventListener('click', () => {
-//   contactMenu.style.display = 'none';
-//   formMenu.style.display = 'none';
-//   listMenu.style.display = 'block';
-//   heading.style.display = 'block';
-//   Link.style.color = 'blue';
-//   contactEl.style.color = '#000';
-//   formLink.style.color = '#000';
-// });
-
-// formLink.addEventListener('click', () => {
-//   contactMenu.style.display = 'none';
-//   formMenu.style.display = 'block';
-//   listMenu.style.display = 'none';
-//   heading.style.display = 'none';
-//   Link.style.color = '#000';
-//   contactEl.style.color = '#000';
-//   formLink.style.color = 'blue';
-// });
-
 const bookList = JSON.parse(localStorage.getItem('storedBooks')) || [];
 
-export class Library {
+class Library {
   constructor(title, author) {
     this.title = title;
     this.author = author;
